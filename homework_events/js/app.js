71fbb9836cba3c8938a37767caf6298c6c13086c
Nathan Langridge <nathanlangridge@hotmail.com>
2019-10-04 11:34:09 +0100

    MVP

---
+++ homework_events/js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const newItemForm = document.querySelector('#new-item-form');
+  newItemForm.addEventListener('submit', handleNewItemFormSubmit);
+
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllClick);
+})
+
+const handleNewItemFormSubmit = function (event) {
+  event.preventDefault();
+
+  const filmItem = createFilmItem(event.target);
+  const film = document.querySelector('#film');
+  film.appendChild(filmItem);
+
+  event.target.reset();
+}
+
+const createFilmItem = function (form) {
+  const filmItem = document.createElement('li');
+  filmItem.classList.add('film-item');
+
+  const title = document.createElement('p');
+  title.textContent = form.title.value;
+  filmItem.appendChild(title);
+
+  const director = document.createElement('p');
+  director.textContent = form.director.value;
+  filmItem.appendChild(director);
+
+  const genre = document.createElement('p');
+  genre.textContent = form.genre.value;
+  filmItem.appendChild(genre);
+
+  return filmItem;
+}
+
+const handleDeleteAllClick = function (event) {
+  const film = document.querySelector('#film');
+  film.innerHTML = '';
+}
---
+++ homework_quiz/homework_quiz.md
@@ -0,0 +1,23 @@
+1. What is the return value of `document.createElement`? And what does it accept as an argument?
+
+  document.createElement returns what you want the HTML element to be.
+  Accepts an HTML element as an argument.
+
+2. What is the return value of `document.querySelector`? And what does it accept as an argument?
+
+  document.querySelector returns the FIRST element in the document that matches what you've specified.
+  Accepts an ID as an argument.
+
+3. What is the return value `document.querySelectorAll`?
+
+  document.querySelectorAll returns ALL the elements in the document that match what you've specified.
+
+4. What is the difference between using `textContent` and `appendChild`?
+
+  textContent REPLACES the contents of an element with a new value.
+  appendChild ADDS without replacing anything.
+
+5. What is the event we handle to access the values of a form, and how do we access the values?
+
+  The event we handle to access the values of a form is interaction via the browser.
+  We access the values via an Event Listener.
